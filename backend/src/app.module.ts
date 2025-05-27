505917239e023882bbe548340b665dd061797bf9
Your Name <your.email@example.com>
2025-05-27 15:09:02 +0700

    update backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { PagesModule } from './pages/pages.module';
+import { CategoriesModule } from './categories/categories.module';
+import { PartnersService } from './partners/partners.service';
+import { PartnersController } from './partners/partners.controller';
+import { PartnersModule } from './partners/partners.module';
+import { SettingsModule } from './settings/settings.module';
+import { FaqsService } from './faqs/faqs.service';
+import { FaqsController } from './faqs/faqs.controller';
+import { FaqsModule } from './faqs/faqs.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    PostsModule,
+    PagesModule,
+    CategoriesModule,
+    PartnersModule,
+    SettingsModule,
+    FaqsModule,
+  ],
+  controllers: [AppController, PartnersController, FaqsController],
+  providers: [AppService, PartnersService, FaqsService],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3001);
+  const config = new DocumentBuilder()
+    .setTitle('Phillip Trustee API')
+    .setDescription('API for dashboard/backend')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 }
 bootstrap();
